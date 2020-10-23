a9149a67a008eef47e5da99003f461c4ab080016
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-10-23 13:43:38 -0500

    Updates

---
+++ precache-manifest.1192af1fd4b6d1c6571e80db6f57359d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcadd24bc98edd59f715e56271dd9e4",
+    "url": "/celebs/index.html"
+  },
+  {
+    "revision": "5ac0caa1304629b7b731",
+    "url": "/celebs/static/css/main.8951ed8e.chunk.css"
+  },
+  {
+    "revision": "94f5a1dff50edf139553",
+    "url": "/celebs/static/js/2.ab826d98.chunk.js"
+  },
+  {
+    "revision": "f17d886c238b2f65956edf0881882911",
+    "url": "/celebs/static/js/2.ab826d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac0caa1304629b7b731",
+    "url": "/celebs/static/js/main.d3ab95d8.chunk.js"
+  },
+  {
+    "revision": "2d89e92baf455edb06cf",
+    "url": "/celebs/static/js/runtime-main.404a3883.js"
+  },
+  {
+    "revision": "8cdeb7a119fb7c4f246e19fff32ed760",
+    "url": "/celebs/static/media/bootstrap.8cdeb7a1.less"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/celebs/precache-manifest.1192af1fd4b6d1c6571e80db6f57359d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/celebs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
